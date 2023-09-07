04d154ebb3889f93471e7c9e0d75729a06690670
Ivan Grginov <ivangrginovbiz@gmail.com>
2023-09-07 21:37:36 +0200

    Test package build

---
+++ src/annoying-favicon.js
@@ -0,0 +1,7 @@
+/**
+ * Do some test stuff.
+ *
+ * @param paramSOmething
+ * @returns {void}
+ */
+export const testStuff = (paramSOmething) => console.log('testing stuff2' + paramSOmething);
---
+++ src/annoying-favicon.ts
@@ -0,0 +1,7 @@
+/**
+ * Do some test stuff.
+ *
+ * @param paramSOmething
+ * @returns {void}
+ */
+export const testStuff = (paramSOmething: string) => console.log('testing stuff2' + paramSOmething);
